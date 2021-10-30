eeabdf5f94c557b9816d7f3f7f8b36026e4d4118
JannatulHappy <jannatulferdoushappy21@gmail.com>
2021-10-31 02:49:46 +0600

    initial-heroku-deploy-done

--- src/Pages/Home/Destination/Destination.js
+++ src/Pages/Home/Destination/Destination.js
@@ -4,22 +4,24 @@ import SingleDestination from "./SingleDestination/SingleDestination";
 const Destination = () => {
   const [destinations, setDestinations] = useState([]);
   useEffect(() => {
-    fetch("./destinations.json")
+    fetch("https://secure-hamlet-63845.herokuapp.com/allDestinations")
       .then((res) => res.json())
       .then((data) => setDestinations(data));
   }, []);
-  
+
   return (
     <div className="destinations">
       <div className="row destination-container my-5">
         <p className="title">FEATURED TOURS</p>
-        <hr className="hr"/>
+        <hr className="hr" />
         <p className="sub-title mt-2">
           Discover the highlights of travelling and find ideas for your holidays
           with these stories.
         </p>
         <div className="row g-4 destination">
-          {destinations.slice(0, 8).map((destination) => <SingleDestination destination={destination}></SingleDestination>)}
+          {destinations.slice(0, 8).map((destination) => (
+            <SingleDestination destination={destination}></SingleDestination>
+          ))}
         </div>
       </div>
     </div>
